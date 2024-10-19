bb6a5239e4e832eb07c28ba84768d8dad6534938
mathiasUCSB <mathiasooi@ucsb.edu>
2024-10-18 18:38:29 -0700

    Initial commit

--- function.rb
+++ function.rb
@@ -5,10 +5,66 @@ require 'jwt'
 require 'pp'
 
 def main(event:, context:)
-  # You shouldn't need to use context, but its fields are explained here:
-  # https://docs.aws.amazon.com/lambda/latest/dg/ruby-context.html
-  response(body: event, status: 200)
+  case event['path']
+  when '/'
+    if event['httpMethod'] == 'GET'
+      return handle_get(event)
+    else
+      return response(body: {error: 'Method Not Allowed'}, status: 405)
+    end
+  when '/auth/token'
+    if event['httpMethod'] == 'POST'
+      return handle_post(event)
+    else
+      return response(body: {error: 'Method Not Allowed'}, status: 405)
+    end
+  end
+
+  response(body: {error: 'Not Found'}, status: 404)
 end
+
+def handle_post(event)
+  # Guard clause for valid content-type
+  unless event['headers']['Content-Type'] == 'application/json'
+      return response(body: {error: 'Unsupported Media Type'}, status: 415)
+  end
+
+  # Try to parse JSON body
+  begin
+    body = JSON.parse(event['body'])
+  rescue
+    return response(body: {error: 'Unprocessable Entity'}, status: 422)
+  end
+
+  # Generate token
+  payload = {
+    data: body,
+    exp: Time.now.to_i + 5,
+    nbf: Time.now.to_i + 2
+  }
+  token = JWT.encode(payload, ENV['JWT_SECRET'], 'HS256')
+
+  return response(body: {token: token}, status: 201)
+end
+
+def handle_get(event)
+  auth_header = event['headers']['Authorization']
+  unless auth_header && auth_header.start_with?('Bearer ')
+    return response(body: {error: 'Forbidden'}, status: 403)
+  end
+
+  begin
+    token = JWT.decode(auth_header.split(' ')[1], ENV['JWT_SECRET'], true, {algorithm: 'HS256'})
+    data = token[0]['data']
+  rescue JWT::ExpiredSignature, JWT::ImmatureSignature
+    return response(body: {error: 'Unauthorized'}, status: 401)
+  rescue JWT::DecodeError
+    return response(body: {error: 'Forbidden'}, status: 403)
+  end
+  
+  response(body: data, status: 200)
+end
+
 
 def response(body: nil, status: 200)
   {
@@ -31,15 +87,15 @@ if $PROGRAM_NAME == __FILE__
                'path' => '/token'
              })
 
-  # Generate a token
+  # # Generate a token
   payload = {
     data: { user_id: 128 },
     exp: Time.now.to_i + 1,
     nbf: Time.now.to_i
   }
   token = JWT.encode payload, ENV['JWT_SECRET'], 'HS256'
-  # Call /
-  PP.pp main(context: {}, event: {
+  # # Call
+  PP.pp main(context: {}, event: {  
                'headers' => { 'Authorization' => "Bearer #{token}",
                               'Content-Type' => 'application/json' },
                'httpMethod' => 'GET',
